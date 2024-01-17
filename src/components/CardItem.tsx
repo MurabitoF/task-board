ee343bf6fe979ebfad6a782811190bd648b84fcb
MurabitoF <francomurabito@outlook.com>
2024-01-17 14:38:25 -0300

    feat: CardItem component

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,55 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+import type { Card } from "../types";
+
+interface Props {
+	data: Card;
+}
+
+const CardItem: React.FC<Props> = ({ data }) => {
+	const {
+		attributes,
+		isDragging,
+		listeners,
+		transition,
+		transform,
+		setNodeRef,
+	} = useSortable({
+		id: data.id,
+		data: {
+			type: "Card",
+			card: data,
+		},
+	});
+
+	const style = {
+		transform: CSS.Translate.toString(transform),
+		transition,
+	};
+
+	if (isDragging) {
+		return (
+			<div
+				ref={setNodeRef}
+				style={style}
+				className="min-h-20 border-2 border-neutral-300 rounded-lg bg-white opacity-70"
+			></div>
+		);
+	}
+
+	return (
+		<li
+			{...attributes}
+			{...listeners}
+			ref={setNodeRef}
+			style={style}
+			className="bg-slate-100 w-full  p-4 rounded-lg flex flex-col border-l-4 border-red-400"
+		>
+			<h3 className="text-xl text-ellipsis overflow-x-hidden">{data.title}</h3>
+			<p className="text-sm text-slate-400 self-end">{data.owner}</p>
+		</li>
+	);
+};
+
+export default CardItem;
