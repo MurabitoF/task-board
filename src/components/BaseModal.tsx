d97a4f440823fc50d72b5f43059b4196108466dc
MurabitoF <francomurabito@outlook.com>
2024-01-18 11:04:05 -0300

    feat: create base modal component

---
+++ src/components/BaseModal.tsx
@@ -0,0 +1,30 @@
+import { MouseEvent, PropsWithChildren, forwardRef } from "react";
+import { createPortal } from "react-dom";
+
+interface Props {
+	className?: string;
+	closeOnClickOutside?: boolean;
+	onClose: () => void;
+}
+
+const BaseModal = forwardRef<HTMLDialogElement, PropsWithChildren<Props>>(
+	({ className, onClose, children, closeOnClickOutside }, dialogRef) => {
+		const handleClick = (e: MouseEvent<HTMLDialogElement>) => {
+			e.stopPropagation();
+			if (e.currentTarget === e.target && closeOnClickOutside) onClose();
+		};
+
+		return createPortal(
+			<dialog
+				ref={dialogRef}
+				onClick={handleClick}
+				className={`fixed inset-0 z-10 overflow-y-auto rounded-lg shadow-md backdrop:bg-neutral-500/30 backdrop:backdrop-blur-sm ${className}`}
+			>
+				<div className="p-4">{children}</div>
+			</dialog>,
+			document.body,
+		);
+	},
+);
+
+export default BaseModal;
