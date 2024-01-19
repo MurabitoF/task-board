e46cf82c24a2d628bf62b3e10f98b765cce11c73
MurabitoF <francomurabito@outlook.com>
2024-01-19 10:14:38 -0300

    feat: added animations to modals

--- src/components/Modals/BaseModal.tsx
+++ src/components/Modals/BaseModal.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { MouseEvent, PropsWithChildren, forwardRef } from "react";
 import { createPortal } from "react-dom";
 
@@ -15,14 +16,19 @@ const BaseModal = forwardRef<HTMLDialogElement, PropsWithChildren<Props>>(
 		};
 
 		return createPortal(
-			<dialog
-				ref={dialogRef}
-				onClick={handleClick}
-				className={`fixed inset-0 z-10 overflow-y-auto rounded-lg shadow-md backdrop:bg-neutral-500/30 dark:backdrop:bg-neutral-900/30 
+			<AnimatePresence>
+				<motion.dialog
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					exit={{ opacity: 0, scale: 0.5 }}
+					ref={dialogRef}
+					onClick={handleClick}
+					className={`fixed inset-0 z-10 overflow-y-auto rounded-lg shadow-md backdrop:bg-neutral-500/30 dark:backdrop:bg-neutral-900/30 
 				backdrop:backdrop-blur-sm dark:bg-neutral-950 dark:text-neutral-100 ${className}`}
-			>
-				<div className="p-4">{children}</div>
-			</dialog>,
+				>
+					<div className="p-4">{children}</div>
+				</motion.dialog>
+			</AnimatePresence>,
 			document.body,
 		);
 	},
